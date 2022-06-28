28a1708b9a48989a9d83c8b9fa1aac7526cdc606
akmalsyrf <dev.akmal69@gmail.com>
2022-06-28 20:55:47 +0700

    register user

---
+++ handler/user.go
@@ -0,0 +1,31 @@
+package handler
+
+import (
+	"bwastartup/user"
+
+	"github.com/gin-gonic/gin"
+)
+
+type userHandler struct {
+	userService user.Service
+}
+
+func NewUserHandler(userService user.Service) *userHandler {
+	return &userHandler{userService}
+}
+
+func (h *userHandler) RegisterUser(c *gin.Context) {
+	var input user.RegisterUserInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+	}
+
+	user, err := h.userService.RegisterUser(input)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+	}
+
+	c.JSON(200, user)
+}
